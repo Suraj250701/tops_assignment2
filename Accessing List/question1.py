0747743f08c561a5c6a223328f2f0a3f79cb7186
Suraj Singh <surajsingh88597@gmail.com>
2025-06-24 14:34:03 +0530

    Accessing List

---
+++ Accessing List/question1.py
@@ -0,0 +1,4 @@
+# Write a Python program to create a list of multiple data type elements
+my_list = [42, "Hello", 3.14, True, None]
+print(my_list)
+
---
+++ Accessing List/question2.py
@@ -0,0 +1,7 @@
+# Write a Python program to find the length of a list using the len() function.
+
+usr_list = [42, "Hello", 3.14, True, None]
+list_length = len(usr_list)
+print(f"The length of the list is: {list_length}")
+
+
