2d159fe2765e74394b8654ec2e076a6411530d5c
Suraj Singh <surajsingh88597@gmail.com>
2025-06-24 17:12:44 +0530

    Dictionaries

---
+++ Dictionaries/Practical Examples/question2.py
@@ -0,0 +1,18 @@
+# Write a Python program to access values using keys from a dictionary.
+
+student_info = {
+    "name": "Suraj Singh",
+    "age": 23,
+    "Degree": "B.Tech",
+    "major": "Information Technology",
+    "university": "Ganpat University",
+    "email": "surajsingh88597@gmail.com"
+}
+
+# Accessing values using keys.
+print("Name : ", student_info["name"])
+print("Age : ", student_info["age"])
+print("Degree : ", student_info["Degree"])
+print("Major : ", student_info["major"])
+print("University : ", student_info["university"])
+print("Email : ", student_info["email"])
---
+++ Dictionaries/question1.py
@@ -0,0 +1,14 @@
+# Write a Python program to create a dictionary with 6 key-value pairs.
+student_info = {
+    "name": "Suraj Singh",
+    "age": 23,
+    "Degree": "B.Tech",
+    "major": "Information Technology",
+    "university": "Ganpat University",
+    "email": "surajsingh88597@gmail.com"
+}
+
+print("Student Information Dictionary : ")
+for key, value in student_info.items():
+    print(f"{key}: {value}")
+print("\nStudent Information Dictionary : ", student_info)
